061b674fd59c8b82bebf7ad7a6581f2938c6d6b8
tracer-ics2019 <tracer@njuics.org>
2019-09-15 11:43:37 +0000

    >  compile
    ics
    Linux 59a74badb336 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux
     11:43:37 up 23:16,  7 users,  load average: 0.00, 0.00, 0.00
    cdb47df0842ce3ed971e5298c86594db5da122ff

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -9,27 +9,35 @@ void ui_mainloop(int);
 /***pa1.2 random test***/
 uint32_t expr(char *e, bool *success);
 
+static void rand_expr_test();
+
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
+
+	/*Random test expr*/
+	rand_expr_test();	
 	
-	/*=============================================*/
-	unsigned val;
-	char str[500];
-	unsigned num;
-	bool success;	
-	for(int i = 0;i < 1;i++){
-		scanf("%u %[^\n]", &num, str);
-		val = expr(str, &success);
-		if(num != val){
-			printf("%d: error\n", i);
-		} else {
-			printf("success%d: %u\n", i, val);
-		}
-	}
-	/*============================================*/
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
   return 0;
+}
+
+static void rand_expr_test(){
+	unsigned val;
+	char str[500];
+	unsigned num;
+	bool success;	
+	for(int i = 0;i < 100;i++){
+		scanf("%u %[^\n]", &num, str);
+		val = expr(str, &success);
+		if(num != val){
+			printf("%d: error\n", i+1);
+		} else {
+			printf("success%d: %u\n", i+1, val);
+		}
+	}
+	/*============================================*/
+
 }
