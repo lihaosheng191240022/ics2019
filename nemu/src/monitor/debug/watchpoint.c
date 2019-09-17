d692e3de419cf8593e42a09734658d798109e52d
tracer-ics2019 <tracer@njuics.org>
2019-09-17 13:15:37 +0000

    >  compile
    ics
    Linux 59a74badb336 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux
     13:15:37 up  2:47,  7 users,  load average: 0.00, 0.01, 0.00
    8e2beeb127dab8d5f30bfa977c55e5282c3800af

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -14,10 +14,34 @@ void init_wp_pool() {
   }
   wp_pool[NR_WP - 1].next = NULL;
 
-  head = NULL;
+  head = (WP *)malloc(sizeof(WP));/*pa1.3: Llist with head node*/
+	head->next = NULL;
   free_ = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP *new_wp(){
+	Assert(free_ != NULL, "watchpoint pool shortage\n");
+	WP *find_tail = head;
+	while(find_tail->next != NULL){
+		find_tail = find_tail->next;
+	}
+	find_tail->next = free_;
+	free_ = free_->next;
+	(find_tail->next)->next = NULL;
+	return find_tail->next;
+}
+
+void free_wp(WP *wp){/*need any check whether wp was exactly in free_ Llist??*/
+	/*delete from head Llist*/
+	WP *before_wp = head;
+	while(before_wp->next != wp){
+		before_wp = before_wp->next;
+	}
+	before_wp->next = wp->next;
+	/*add to free_ Llist*/
+	wp->next = free_;
+	free_ = wp;
+}
 
