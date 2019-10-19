22bd125363d442d12df3fc92df366d185b8d4d9a
171820548-张蹇 <2776046567@qq.com>
2019-10-19 14:50:42 +0000

    PA2.2: done all setxx in cc.c

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -14,14 +14,27 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+    /*pa2.2 add.c*/
+		/*use t0,t1 to store flag bit, ----no need for O,B,E*/
+		case CC_O:	rtl_get_OF(dest);/*if OF==1, set 1; else set 0*/break;
+    case CC_B:	rtl_get_CF(dest);/*if CF==1, set 1; else set 0*/break;
+    case CC_E:	rtl_get_ZF(dest);/*if ZF==1, set 1; else set 0*/break;
+    case CC_BE:	rtl_get_CF(&t0);
+								rtl_get_ZF(&t1);
+								t0 |= t1;
+								*dest = t0;/*if CF|ZF==1, set 1; else set 0*/break;
+    case CC_S:	rtl_get_SF(dest);/*if SF==1, set 1; else set 0*/break;
+    case CC_L:	rtl_get_SF(&t0);
+								rtl_get_OF(&t1);
+								t0 ^= t1;
+								*dest = t0;/*if SF^OF==1, set 1; else set 0*/break;
+    case CC_LE:	rtl_get_SF(&t0);
+								rtl_get_OF(&t1);
+								t0 ^= t1;
+								rtl_get_ZF(&t1);
+								t0 |= t1;
+								*dest = t0;/*if (SF^OF)|ZF==1, set 1; else set 0*/break;
+      
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
