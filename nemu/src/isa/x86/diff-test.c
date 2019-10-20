9c107e07ff4bbedd280007b9f61d08badaf67669
tracer-ics2019 <tracer@njuics.org>
2019-10-20 09:05:49 +0000

    >  compile
    ics
    Linux 59a74badb336 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux
     09:05:49 up 1 day,  9:59, 31 users,  load average: 0.00, 0.00, 0.00
    2fabdd4be8b1cfc5527712958d2b57da9f396605

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -1,9 +1,19 @@
 #include "nemu.h"
 #include "monitor/diff-test.h"
 
+extern void difftest_getregs(void *r);
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
  //TODO()
- 	return false;
+	uint32_t r[9];/*8 gpr and pc*/
+	difftest_getregs(r);
+	CPU_state *ref_ptr = ref_r;
+	for(int i=0;i<9;i++){
+		if(r[i]!=*((uint32_t *)ref_ptr+i)){
+			return false;
+		}
+	}
+ 	return true;
 }
 
 void isa_difftest_attach(void) {
