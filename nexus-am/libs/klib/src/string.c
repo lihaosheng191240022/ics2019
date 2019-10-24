ac01257f013492459a783ab4df92ff0831e4ec7c
tracer-ics2019 <tracer@njuics.org>
2019-10-24 21:24:43 +0000

    >  compile
    ics
    Linux 59a74badb336 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux
     21:24:43 up 1 day,  5:18, 31 users,  load average: 0.06, 0.02, 0.00
    d83dec204af0e0c1496330cfb6bf44db7ba98a56

---
+++ nexus-am/libs/klib/src/string.c
@@ -0,0 +1,50 @@
+#include "klib.h"
+
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+size_t strlen(const char *s) {
+  assert(0);
+	return 0;
+}
+
+char *strcpy(char* dst,const char* src) {
+  assert(0);
+	return NULL;
+}
+
+char* strncpy(char* dst, const char* src, size_t n) {
+  assert(0);
+  return NULL;
+}
+
+char* strcat(char* dst, const char* src) {
+  assert(0);
+  return NULL;
+}
+
+int strcmp(const char* s1, const char* s2) {
+  assert(0);
+  return 0;
+}
+
+int strncmp(const char* s1, const char* s2, size_t n) {
+  assert(0);
+  return 0;
+}
+
+void* memset(void* v,int c,size_t n) {
+  assert(0);
+  return NULL;
+}
+
+void* memcpy(void* out, const void* in, size_t n) {
+  assert(0);
+  return NULL;
+}
+
+int memcmp(const void* s1, const void* s2, size_t n){
+  assert(0);
+  return 0;
+}
+
+#endif
---
+++ nexus-am/tests/cputest/tests/aaa.c
@@ -0,0 +1,10 @@
+#include"trap.h"
+
+//int add(int a, int b){
+//	return a+b;
+//}
+int main(void){
+	//int a=1, b=2;
+	//nemu_assert(a+b==3);	
+	return 0;
+}
