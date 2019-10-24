a2416da69eaf708608b497df6d0eb7a23654032b
tracer-ics2019 <tracer@njuics.org>
2019-10-24 17:10:13 +0000

    >  compile
    ics
    Linux 59a74badb336 4.9.125-linuxkit #1 SMP Fri Sep 7 08:20:28 UTC 2018 x86_64 GNU/Linux
     17:10:13 up 1 day,  1:03, 30 users,  load average: 0.04, 0.01, 0.00
    186a2c751baeb602d0f7dd8669b136af63b2e732

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -135,7 +135,7 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   /*pa2.2*/
 	rtl_li(&t0, *src1);
 	*dest = ~t0;
-	Assert((*dest ^ *src1)==0, "rtl_not is wrong\n");
+	Assert(((*dest) ^ (*src1))==0, "rtl_not is wrong\n");
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
