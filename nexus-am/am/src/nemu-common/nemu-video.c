e62d37f242e59411e8121ad960dcbe14d70d1835
tracer-ics2019 <tracer@njuics.org>
2019-11-06 19:59:08 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     19:59:08 up 2 days,  4:06, 21 users,  load average: 0.03, 0.14, 0.12
    28754de951ae70e22e194107386c51c8d708a47

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -60,8 +60,9 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 void __am_vga_init() {
 	int i;
 	int size = screen_width() * screen_height();
-	printf("size=%d\n", size);
+	//printf("size=%d\n", size);
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+	printf("FB_ADDR=%x\n", FB_ADDR);
 	for(i = 0; i < size; i++){
 		fb[i] = 0xff00;
 	}
