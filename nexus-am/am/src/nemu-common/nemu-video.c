7498e33c462eeaa44970e6f5606898865c07f19a
tracer-ics2019 <tracer@njuics.org>
2019-11-06 19:28:43 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     19:28:43 up 2 days,  3:35, 21 users,  load average: 0.12, 0.14, 0.17
    722fb97819b700b8af05d70760a969c090d8b2db

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -2,12 +2,21 @@
 #include <amdev.h>
 #include <nemu.h>
 
+#define W 400
+#define H 300
+#define FPS 30
+
+//test
+extern int screen_width();
+extern int screen_height();
+extern void draw_sync();
+
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
+      info->width = W;
+      info->height = H;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -18,6 +27,12 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+			//int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+			//uint32_t *pixels = ctl->pixels;
+			//int cp_bytes = sizeof(uint32_t) * min(W, W-x);
+			//for(int j = 0; j < h && y + j < H; j++){
+				//memcpy(&)
+			//}
 
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
@@ -29,4 +44,12 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_vga_init() {
+	int i;
+	int size = screen_width() * screen_height();
+	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+	for(i = 0; i < size; i++){
+		fb[i] = i;
+	}
+	draw_sync();
+
 }
