3f86d377fef15c95c4c7883f7a8cd21ceb921246
tracer-ics2019 <tracer@njuics.org>
2019-11-17 13:57:17 +0800

    >  compile
    bryce
    Linux LittleMonster 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     13:57:17 up 1 day,  9:02, 15 users,  load average: 0.34, 0.13, 0.04
    4aef249e8c879856a6b7458aa4f50e7aa6444b

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -1,10 +1,14 @@
 #include "rtl/rtl.h"
-
 void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-	printf("\033[32m todo in raise_intr\n");
+	//printf("\033[32m todo in raise_intr\n");
+	s0 = cpu.idtr.Base + 8 * NO;
+	s1 = 0x0000ffff & vaddr_read(s0, 4);
+	s0 += 4;
+	s1 |= 0xffff0000 & vaddr_read(s0, 4);
+	cpu.pc = s1;
 }
 
 bool isa_query_intr(void) {
