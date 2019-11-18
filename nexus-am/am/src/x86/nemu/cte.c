e43a8c0bad2a25e97f4879a81cc079d53d207266
tracer-ics2019 <tracer@njuics.org>
2019-11-18 09:44:52 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     09:44:52 up 2 days,  4:15, 16 users,  load average: 0.71, 0.21, 0.11
    7a9f3d5a57fa8f808ab33296dc840bc0fa332abf

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -18,10 +18,13 @@ void __am_vecnull();
 
 _Context* __am_irq_handle(_Context *c) {
   _Context *next = c;
-  if (user_handler) {
+	
+	if (user_handler) {
     _Event ev = {0};
     switch (c->irq) {
-      default: ev.event = _EVENT_ERROR; break;
+      default: 
+							printf("irq=%x\n", c->irq);
+							ev.event = _EVENT_ERROR; break;
     }
 
     next = user_handler(ev, c);
