43d99c08a5ddb2d6cd620907ae6e43628dba7f82
tracer-ics2019 <tracer@njuics.org>
2019-11-18 13:05:53 +0800

    >  compile
    bryce
    Linux LittleMonster 5.0.0-32-generic #34~18.04.2-Ubuntu SMP Thu Oct 10 10:36:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     13:05:53 up 2 days,  5:26, 18 users,  load average: 0.30, 0.28, 0.18
    411669ec1631c3c9309835626441e17e40fd39

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -10,6 +10,7 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
 	s1 |= 0xffff0000 & vaddr_read(s0, 4);
 	printf("\033[36m exception entry = %08x\n\033[0m", s1);
 	rtl_j(s1);
+	rtl_j(ret_addr);
 }
 
 bool isa_query_intr(void) {
