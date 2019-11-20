bf12cad964d802be9657188b1b2b69a38eac97c4
tracer-ics2019 <tracer@njuics.org>
2019-11-20 18:27:03 +0800

    >  compile
    bryce
    Linux LittleMonster 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     18:27:03 up  7:29, 17 users,  load average: 0.05, 0.07, 0.02
    c209ee1132d07788544372194550e3cdb846473

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -26,7 +26,8 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
 #ifdef MYDEBUG
 	_my_debug_ printf("after jmp to __am_vectrap: cpu.pc=%08x\n", cpu.pc);
 #endif
-	printf("after jmp: seq_pc=%08x\n", ret_addr);
+	//printf("after jmp: seq_pc=%08x\n", ret_addr);
+	cpu.pc = ret_addr;
 }
 
 bool isa_query_intr(void) {
