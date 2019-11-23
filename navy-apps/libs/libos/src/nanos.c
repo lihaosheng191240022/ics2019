6353bd624fb9721fb2dde86db163ac7be8bbe3fa
tracer-ics2019 <tracer@njuics.org>
2019-11-23 20:53:50 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     20:53:50 up 1 day, 22:18, 23 users,  load average: 0.25, 0.10, 0.03
    97f9969c5f165b3435182f4fce67df9b4260d50

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -59,7 +59,9 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _exit(SYS_write);
+  //pa3.2
+	_syscall_(SYS_write, fd, buf, count);
+	_exit(SYS_write);
   return 0;
 }
 
