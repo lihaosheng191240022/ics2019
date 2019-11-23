16c4e3201146ed15afddeab3c7e1f66d767ecfae
tracer-ics2019 <tracer@njuics.org>
2019-11-23 21:02:32 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     21:02:32 up 1 day, 22:27, 23 users,  load average: 0.44, 0.14, 0.04
    aa5d6b40667a7cf63db4eb3c2151bd943b9982

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,11 +4,16 @@
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+	//pa3.2
+	a[1] = c->GPR2;
+	a[2] = c->GPR3;
+	a[3] = c->GPR4;
 
   switch (a[0]) {
 		case SYS_exit:	_my_debug_ printf("next: handle SYS_exit\n"); _halt(0); break;
 		case SYS_yield:	_my_debug_ printf("next: handle SYS_yield\n"); _yield(); break;
-	
+		case SYS_write:	_my_debug_ printf("next: handle SYS_write\n");
+									 _halt(0);break;	
 		default: panic("Unhandled syscall ID = %d", a[0]);break;
   }
 
