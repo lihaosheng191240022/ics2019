d9cbd05e93874a9707ad0b56dab1e8b258badb1c
tracer-ics2019 <tracer@njuics.org>
2019-11-24 17:05:38 +0800

    >  compile
    bryce
    Linux LittleMonster 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     17:05:38 up 2 days, 10:58,  8 users,  load average: 0.35, 1.19, 0.98
    a5907b8c4f54784e283fd2068a86f911d5697d1

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -9,21 +9,21 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	uint32_t r[10];/*8 gpr and pc and EFLAGS*/
 	difftest_getregs(r);
 	CPU_state *ref_ptr = ref_r;
-	//int flag = 0;
+	int flag = 0;
 	int i;
 	for(i=0;i<9;i++){
-		//if(r[i]!=*((uint32_t *)ref_ptr+i)){
-		//	printf("diff:%s\n%08x in NEMU\n%08x in QEMU\n", regsl[i], r[i], *((uint32_t *)ref_ptr+i));
-		//	flag = 1;
-		//}
-		Assert(r[i]==(*((uint32_t *)ref_ptr+i)), "diff:%s in NEMU and QEMU\n", regsl[i]);
+		if(r[i]!=*((uint32_t *)ref_ptr+i)){
+			printf("diff:%s\n%08x in NEMU\n%08x in QEMU\n", regsl[i], r[i], *((uint32_t *)ref_ptr+i));
+			flag = 1;
+		}
+		//Assert(r[i]==(*((uint32_t *)ref_ptr+i)), "diff:%s in NEMU and QEMU\n", regsl[i]);
 	}
 	//compare EFLAGS
 	uint32_t eflags_QEMU = *((uint32_t *)ref_ptr + i);
-	Assert((r[9]&0x40) == (eflags_QEMU&0x40), "diff: ZF in NEMU and QEMU\n");
-	Assert((r[9]&0x80) == (eflags_QEMU&0x80), "diff: SF in NEMU and QEMU\n");
+	//Assert((r[9]&0x40) == (eflags_QEMU&0x40), "diff: ZF in NEMU and QEMU\n");
+	//Assert((r[9]&0x80) == (eflags_QEMU&0x80), "diff: SF in NEMU and QEMU\n");
 
-	/*
+	
 	if((r[9]&0x00000040) != (eflags_QEMU&0x00000040)){//ZF=6
 		printf("diff:ZF differ\n");
 		printf("in QEMU, ZF=%d\n", ((eflags_QEMU&0x00000040)==0)?(0):(1));
@@ -36,9 +36,9 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 		printf("in NEMU, SF=%d\n", ((r[9]&0x00000080)==0)?(0):(1));
 		flag = 1;
 	}
-	*/
-	return true;
- 	//return (flag==0)?(true):(false);
+	
+	//return true;
+ 	return (flag==0)?(true):(false);
 }
 
 void isa_difftest_attach(void) {
