da0e2c2ff7e3c614fa8f7be0cd6841bbc6888a98
tracer-ics2019 <tracer@njuics.org>
2019-11-30 15:57:09 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     15:57:09 up 5 days, 17:20, 12 users,  load average: 0.74, 0.78, 0.73
    52d16f108e71193c28a3ce5beef2f067f076c977

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -18,7 +18,8 @@ _Context* do_syscall(_Context *c) {
 		case SYS_yield:	_my_debug_ printf("next: handle SYS_yield\n"); _yield(); 
 									break;
 		case SYS_open: _my_debug_ printf("next: handle SYS_open\n");
-									fs_open((char *)a[1], (int)a[2], (int)a[3]);
+									c->GPRx = fs_open((char *)a[1], (int)a[2], (int)a[3]);
+									_my_debug_ printf("open fd = %d\n", c->GPRx);
 									break;
 		case SYS_write:	_my_debug_ printf("next: handle SYS_write\n");
 										_my_debug_ printf("fd=%d, count=%d\n", a[1], a[3]);
