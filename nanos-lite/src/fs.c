0db4712989f7f50e560ed65748518f24867b8c99
tracer-ics2019 <tracer@njuics.org>
2019-12-01 15:17:42 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     15:17:42 up 6 days,  6:53, 13 users,  load average: 0.96, 0.40, 0.36
    64c942651935988de3996548ad30b17bc90c6192

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -82,7 +82,7 @@ size_t fs_lseek(int fd, size_t offset, int whence){
                                   break;
     case SEEK_CUR:  file_table[fd].open_offset  += offset;
                                   break;
-    case SEEK_END:  file_table[fd].open_offset = file_table[fd].size + offset;
+    case SEEK_END:  assert(0); file_table[fd].open_offset = file_table[fd].size + offset;
                                   break;
     default:                assert(0);
       
