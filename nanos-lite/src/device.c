81e05154e6f361472b62b1a56beb6921f5177d35
tracer-ics2019 <tracer@njuics.org>
2019-12-01 18:46:03 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     18:46:03 up 6 days, 10:21, 13 users,  load average: 0.17, 0.12, 0.12
    fd88829771c5786e279fb5a5356dc6cb7a2bb9

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -36,7 +36,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 	char t[16] = {'\0'};
 	size_t i = 0;
 	while(current_time!=0){
-		t[i] = current_time%10;
+		t[i] = '0'+current_time%10;
 		current_time /= 10;
 		i++;
 	}
