e8148061406d914dab4cd5baa32789530cff86ab
tracer-ics2019 <tracer@njuics.org>
2019-12-01 14:19:06 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     14:19:06 up 6 days,  5:54, 12 users,  load average: 0.87, 0.67, 0.43
    f3da696e210d5d6f593abe4faf338b2892c21fe5

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -9,6 +9,7 @@ typedef struct {
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
@@ -41,7 +42,7 @@ void init_fs() {
 int fs_open(const char *pathname, int flags, int mode){
   for(int i=0;i<NR_FILES;i++){
     if(strcmp(pathname, file_table[i].name)==0){
-      file_table[i].disk_offset = 0;
+      file_table[i].open_offset = 0;
       return i;
     }
   }
@@ -50,7 +51,7 @@ int fs_open(const char *pathname, int flags, int mode){
 
 int fs_close(int fd){
   assert(fd>=0&&fd<NR_FILES);
-  file_table[fd].disk_offset = 0;
+  file_table[fd].open_offset = 0;
   return 0;
 }
 
@@ -59,28 +60,28 @@ extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t fs_read(int fd, void *buf, size_t len){
   assert(fd>=0&&fd<NR_FILES);
   /*read from file to buf*/
-  return ramdisk_read(buf, file_table[fd].disk_offset, len);
+  return ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   
   //return 0;
 }
 
 size_t fs_write(int fd, void *buf, size_t len){
   assert(fd>=0&&fd<NR_FILES);
-  return ramdisk_write(buf, file_table[fd].disk_offset, len);
+  return ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence){
   assert(fd>=0&&fd<NR_FILES);
 
   switch(whence){
-    case SEEK_SET:  file_table[fd].disk_offset = offset; 
+    case SEEK_SET:  file_table[fd].open_offset = offset; 
                                   break;
-    case SEEK_CUR:  file_table[fd].disk_offset  += offset;
+    case SEEK_CUR:  file_table[fd].open_offset  += offset;
                                   break;
-    case SEEK_END:  file_table[fd].disk_offset = file_table[fd].size + offset;
+    case SEEK_END:  file_table[fd].open_offset = file_table[fd].size + offset;
                                   break;
     default:                assert(0);
       
   }
-  return file_table[fd].disk_offset;
+  return file_table[fd].open_offset;
 }
