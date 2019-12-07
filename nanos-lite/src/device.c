7b5c39b4cb7c182f82d070e21802465e76d6764d
tracer-ics2019 <tracer@njuics.org>
2019-12-07 23:59:41 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     23:59:41 up 9 days, 19:22, 13 users,  load average: 1.10, 0.88, 0.67
    c7f1b55285c1c92134677b36c1dae2df50e1f6e

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -92,6 +92,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 	//if(len>128){
 	//	len = 128;
 	//}
+	assert(0);
 	printf("offset=%u, len=%u\n", offset, len);
 	printf("dispinfo=%s", dispinfo);
 	memcpy(buf, dispinfo, len);
@@ -106,6 +107,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
+  assert(0);
   draw_sync();
 	return len;
 }
