13b39f48114b0c565cd80a510abe38f190aa1f7b
tracer-ics2019 <tracer@njuics.org>
2019-12-08 12:52:06 +0800

    >  run
    bryce
    Linux LittleMonster 5.0.0-36-generic #39~18.04.1-Ubuntu SMP Tue Nov 12 11:09:50 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
     12:52:06 up 9 days, 23:19, 13 users,  load average: 0.80, 0.98, 0.81
    2cbd282a56ecd4b3de42f692a6f02cec907bbf07

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -134,13 +134,16 @@ static void get_display_info() {
   screen_w = screen_h = 0;
   char buf[128], key[128], value[128], *delim;
   while (fgets(buf, 128, dispinfo)) {
-    printf("%s\n", buf);//debug
+    //printf("%s\n", buf);//debug
 		*(delim = strchr(buf, ':')) = '\0';
     sscanf(buf, "%s", key);
     sscanf(delim + 1, "%s", value);
     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
-  }
+		
+		//debug
+		if(screen_w!=0 && screen_h!=0) break;
+	}
   fclose(dispinfo);
   assert(screen_w > 0 && screen_h > 0);
 }
